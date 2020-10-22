2650f9a41e3a7ad211ac55dc967008c184c107b3
michmitz <stermitzm@gmail.com>
2020-10-21 17:58:44 -0700

    Added Articles component with test

---
+++ src/components/Articles/Articles.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Article from '../Article/Article';
+
+const Articles = ({ articles }) => {
+  const articleslist = articles.map(article => (
+    <li key={article.title}>
+        <Article {...article} />
+    </li>
+  ));
+
+  return (
+    <ul data-testid="articles">
+      {articleslist}
+    </ul>
+  );
+};
+
+Articles.propTypes = {
+  articles: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired
+  })).isRequired
+};
+
+export default Articles;
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Loading from '../loading/Loading';
-import Article from './Article'
+import Article from '../Article/Article'
 
 export default function App() {
   return <Loading />
